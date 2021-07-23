89cf425bbfac32dbfaf856be17191fd9f7e6bf9f
Matt Robinson <matt@inanimatt.com>
2021-07-23 14:11:03 +0100

    Give up on vue/compat

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,7 @@ import { defineConfig } from 'vite'
 import createVuePlugin from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      vue: '@vue/compat'
-    }
-  },
   plugins: [
-    createVuePlugin({
-      template: {
-        compilerOptions: {
-          compatConfig: {
-            MODE: 2
-          }
-        }
-      }
-    })
+    createVuePlugin(),
   ]
 });
